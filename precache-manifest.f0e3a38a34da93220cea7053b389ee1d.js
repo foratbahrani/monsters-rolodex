50982e8415873f41b5e67e2587ab7a302f9e8220
Forat <foratbahrani@icloud.com>
2020-03-18 10:15:16 +0300

    Updates

---
+++ precache-manifest.f0e3a38a34da93220cea7053b389ee1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a0e23e4b674d426d0b372c7e4950a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "250492f9839b34417501",
+    "url": "/monsters-rolodex/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7223e0e2c67305bf514e",
+    "url": "/monsters-rolodex/static/css/main.e61585e4.chunk.css"
+  },
+  {
+    "revision": "250492f9839b34417501",
+    "url": "/monsters-rolodex/static/js/2.04a385ea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.04a385ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7223e0e2c67305bf514e",
+    "url": "/monsters-rolodex/static/js/main.980910ee.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
